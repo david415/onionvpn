a788be92048148efed474912580714219c31674f
David Stainton <dstainton415@gmail.com>
2013-10-03 21:12:18 -0700

    IPPacketWriter deserves it's own file

--- hush_writer.py
+++ hush_writer.py
@@ -12,6 +12,8 @@ import struct
 from tun_factory import TUNFactory
 from tun_reader import TUNPacketProducer, TUN_TestConsumer
 from tun_writer import TUN_TestProducer
+from ip_packet_writer import IPPacketWriter
+
 
 class HushPacketMTUException(Exception):
      pass
@@ -61,44 +63,6 @@ class HushPacketConsumer(object):
           encoded_packet = ip/tcp
           return encoded_packet
 
-
-class IPPacketWriter(object):
-
-     implements(interfaces.IWriteDescriptor)
-
-     def __init__(self, dest_ip):
-          self.dest_ip = (dest_ip, 0)
-          self.socket  = socket(AF_INET, SOCK_RAW, IPPROTO_RAW)
-          self.socket.setsockopt(SOL_IP, IP_HDRINCL, 1)
-          self.socket.setblocking(0)
-          self.packets = []
-
-     def start(self):
-          reactor.addWriter(self)
-
-     def stop(self):
-          reactor.removeWriter(self)
-
-     def write(self, packet):
-          if len(self.packets) == 0:
-               reactor.addWriter(self)
-          self.packets.append(packet)
-
-     def fileno(self):
-          return self.socket.fileno()
-
-     def connectionLost(self, reason):
-          reactor.removeWriter(self)
-          return reason
-
-     def doWrite(self):
-          if len(self.packets) > 0:
-               self.socket.sendto(self.packets.pop(0), self.dest_ip)
-          if len(self.packets) == 0:
-               reactor.removeWriter(self)
-
-     def logPrefix(self):
-         return 'IPPacketWriter'
 
 
 def main():
