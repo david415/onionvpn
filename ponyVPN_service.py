bcee0ccf372cdee8fcfce8e6ad229625ef0be7b4
David Stainton <dstainton415@gmail.com>
2015-10-05 21:37:19 +0200

    Add rough draft high level for tor onion vpn

--- ponyVPN_service.py
+++ ponyVPN_service.py
@@ -7,38 +7,28 @@ import pytun
 
 # Internal modules
 from tun_protocol import TunProducerConsumer
-from icmp_readerwriter import ICMPReaderWriter
+from onion_readerwriter import OnionProducer
 from twisted.pair.tuntap import TuntapPort
 
 
 class PonyVPNService(service.Service):
 
-    def __init__(self, 
-                 tun_name    = None,
-                 remote_ip   = None,
-                 local_ip    = None):
-
-        self.tun_name    = tun_name
-        self.remote_ip   = remote_ip
-        self.local_ip    = local_ip
+    def __init__(self, tun_name, tor_control_port, onion_key_file):
+        self.tun_name = tun_name
+        self.tor_control_port = tor_control_port
+        self.onion_key_file = onion_key_file
 
     def startService(self):
-
         tun_protocol = TunProducerConsumer()
+        onion_producer_protocol = OnionProducer(consumer = tun_protocol)
+        ipv6_onion_consumer = IPv6OnionConsumer()
+        tun_protocol.setConsumer(ipv6_onion_consumer)
+        onion_endpoint = serverFromString("onion:80:controlPort=%s:hiddenServiceKeyFile=%s" % (self.tor_control_port, self.onion_key_file))
+        d = onion_endpoint.listen(onion_producer_protocol)
+        d.addCallback()
 
-        icmp_ConsumerProducer = ICMPReaderWriter(
-            consumer    = tun_protocol, 
-            local_ip    = self.local_ip,
-            remote_ip   = self.remote_ip)
-
-        tun_protocol.setConsumer(icmp_ConsumerProducer)
-
-        tun = TuntapPort(b"tun0", tun_protocol, reactor=reactor)
-
+        tun = TuntapPort(self.tun_name, tun_protocol, reactor=reactor)
         tun.startListening()
-        icmp_ConsumerProducer.start()
 
     def stopService(self):
         pass
-
-
