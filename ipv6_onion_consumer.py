3c7c99c98ff8e06160fdb5b259b0fa660f33b139
David Stainton <dstainton415@gmail.com>
2015-10-05 23:52:02 +0200

    Add rough ipv6 packet consumer to send to onion services

---
+++ ipv6_onion_consumer.py
@@ -0,0 +1,77 @@
+from scapy.all import IP
+from zope.interface import implementer
+import socket
+#from txtorcon.endpoints import TorOnionAddress
+
+from twisted.internet import interfaces
+from twisted.internet import defer
+from twisted.python import log
+
+
+def convert_ipv6_to_onion(ipv6_addr):
+    packet = socket.inet_pton(socket.AF_INET6, ipv6_addr)
+    return packet[:10]
+
+class PooledOnionFactory(Factory):
+    def __init__(self):
+        super(Factory, self).__init__()
+        self.pool = set()
+
+    def buildProtocol(self, addr):
+        print "PooledOnionFactory buildProtocol"
+        # XXX todo: assert type TorOnionAddress
+        print "------- addr onion uri == %s" % (addr.onion_uri,)
+        # XXX correct?
+        p = self.protocol()
+        self.pool[addr.onion_uri] = p
+        p.factory = self
+        return p
+
+@implementer(interfaces.IConsumer)
+class Ipv6OnionConsumer(object):
+
+    def __init__(self):
+        super(Ipv6OnionConsumer, self).__init__()
+        self.pooledOnionFactory = PooledOnionFactory()
+
+    def logPrefix(self):
+        return 'Ipv6OnionConsumer'
+
+    def getOnionConnection(self, onion):
+        """ getOnionConnection returns a deferred which fires
+        with a client connection to an onion address.
+        """
+        if onion in self.pooledOnionFactory.pool:
+            return defer.succeed(self.pooledOnionFactory.pool[onion])
+        else:
+            torEndpoint = clientFromString("tor:%s.onion" % onion)
+            d = torEndpoint.connect(self.pooledOnionFactory)
+            return d
+
+    # IConsumer section
+    def write(self, packet):
+        try:
+            ip_packet = IP(datagram)
+            assert ip_packet.version == 6
+            # XXX assert that the source address is correct?
+
+            onion = convert_ipv6_to_onion(ip_packet.dst)
+            d = self.getOnionConnection(onion)
+            d.addCallback(lambda p: p.transport.write(packet))
+            # XXX dropped deferred
+
+        except struct.error:
+            log.msg("not an ip packet")
+
+    def registerProducer(self, producer, streaming):
+        log.msg("registerProducer")
+        assert self.producer is None
+        assert streaming is True
+
+        self.producer = producer
+        self.producer.resumeProducing()
+
+    def unregisterProducer(self):
+        log.msg("unregisterProducer")
+        assert self.producer is not None
+        self.producer.stopProducing()
