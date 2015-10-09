a86a5138df1bc3fdb76b0c6b8cc666cc3b4fdcdc
David Stainton <dstainton415@gmail.com>
2015-10-09 16:35:31 +0200

    Teach tcp frame producer to use ipv6 header payload field len
    here we use the obfsproxy network.buffer.Buffer class to help us
    buffer packet fragments...

--- tcp_frame_producer.py
+++ tcp_frame_producer.py
@@ -2,13 +2,18 @@
 import struct
 
 from zope.interface import implementer
-
 from twisted.internet import interfaces
 from twisted.protocols.basic import Int16StringReceiver
 from twisted.internet.protocol import Protocol, Factory
 from twisted.python import log
 from scapy.all import IPv6, hexdump
+from struct import unpack
 
+from buffer import Buffer
+
+
+IPV6_HEADER_LEN = 40
+MAX_FRAME_LEN = 65580
 
 class PersistentSingletonFactory(Factory):
     def __init__(self, protocol):
@@ -40,34 +45,72 @@ class PersistentProtocol(Protocol):
         print "connectionLost %r" % (reason,)
 
 @implementer(interfaces.IPushProducer)
-class TcpFrameProducer(Int16StringReceiver, object):
+class TcpFrameProducer(Protocol, object):
     def __init__(self, local_addr, consumer=None):
         super(TcpFrameProducer, self).__init__()
         print "TcpFrameProducer init"
         self.local_addr = local_addr
         self.consumer = consumer
 
-    def stringReceived(self, data):
-        """
-        Called with the packet from the onion interface after it is deframed
-        """
-        # XXX: This isn't being called when the data isn't framed
-        print "stringReceived"
+        self.frag_buffer = Buffer()
+        self.frag_target_size = -1
+
+    def dataReceived(self, data):
+        packet = None
+        if self.frag_buffer:
+            if len(data) < self.frag_target_size:
+                self.frag_buffer.write(data)
+                self.frag_target_size = self.frag_target_size - len(data)
+            elif len(data) == self.frag_target_size:
+                self.filter_send(self.frag_buffer.drain() + data)
+                self.frag_target_size = -1
+            else: # len(data) > self.frag_target_size
+                self.filter_send(self.frag_buffer.drain() + data[:self.frag_target_size])
+                self.frag_buffer.write(data[self.frag_target_size:])
+                ip_payload_len = unpack('!H', data[self.frag_target_size:])
+                print "unpacked ipv6 payload len %r" % (ip_payload_len,)
+                if ip_payload_len > MAX_FRAME_SIZE - IPV6_HEADER_LEN:
+                    print "max frame size exceeded in overlap read op"
+                    print "dropping data..."
+                    self.frag_buffer.drain()
+                    self.frag_target_size = -1
+                else:
+                    self.frag_target_size = ip_payload_len + IPV6_HEADER_LEN
+        else:
+            ip_payload_len = unpack('!H', data[4:6]) # IPv6 header field for payload len
+            if ip_payload_len > MAX_FRAME_SIZE - IPV6_HEADER_LEN:
+                print "max frame size exceeded in read op"
+                print "dropping data..."
+                return
+            print "unpacked ipv6 payload len %r" % (ip_payload_len,)
+            self.current_frag_len = IPV6_HEADER_LEN + ip_payload_len
+            if len(data) < self.current_frag_len:
+                self.frag_buffer.write(data)
+                self.frag_target_size = self.current_frag_len - len(data)
+            elif len(data) == self.current_frag_len:
+                self.filter_send(packet)
+            else: # len(data) > self.current_frag_len
+                ip_payload_len = unpack('!H', data[4:6])
+                self.filter_send(data[:ip_payload_len + IPV6_HEADER_LEN])
+                self.frag_buffer.write(data[ip_payload_len + IPV6_HEADER_LEN:])
+
+    def filter_send(self, packet):
         # assert that it's an IPv6 packet
         try:
             print "valid IPv6 packet"
-            packet = IPv6(data)
+            ipv6_packet = IPv6(packet)
         except struct.error:
             print "not an IPv6 packet"
-            log.msg("not an IPv6 packet")
+            log.msg("not sending IPv6 packet")
+            return # not-send non-ipv6 packets
 
         # assert that the destination IPv6 address matches our address
-        if packet.dst != self.local_addr:
+        if ipv6_packet.dst != self.local_addr:
             log.msg("packet destination doesn't match our vpn destination")
 
         # write the IPv6 packet to our consumer
         print "writing to consumer now"
-        self.consumer.write(data)
+        self.consumer.write(packet)
 
     def logPrefix(self):
         return 'OnionProducer'
